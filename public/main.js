37d236f6730a262a2b866a941f2ea8f7378a0f5e
Amanda Leaders <alsleaders@gmail.com>
2019-05-09 15:35:48 -0400

    first card display works, do second next

--- public/main.js
+++ public/main.js
@@ -16,6 +16,7 @@ const faces = [
 ]
 
 let cardDeck = []
+// let shownDeck = []
 
 const main = () => {
   if (document.querySelector('h1.hello-world')) {
@@ -24,10 +25,10 @@ const main = () => {
 }
 
 const createDeck = () => {
-  for (let i = 0; i > suits.length; i++) {
+  for (let i = 0; i < suits.length; i++) {
     const s = suits[i]
     console.log({ s }) // should say the suits
-    for (let j = 0; j > faces.length; j++) {
+    for (let j = 0; j < faces.length; j++) {
       const f = faces[j]
       console.log({ f }) // should say the face
       const card = {
@@ -37,9 +38,38 @@ const createDeck = () => {
         imageUrl: faces[j].rank + ' of ' + suits[i] + '.png'
       }
       cardDeck.push(card)
-      console.log(card)
+      console.log({ card })
     }
   }
+  console.log('All cards pushed to deck')
 }
+const shuffle = () => {
+  // shownDeck = []
+  for (let i = cardDeck.length - 1; i > 0; i--) {
+    // select a random card we have not hit yet
+    const randomLocation = Math.floor(Math.random() * (i + 1))
+    // swap the current card with the random card
+    let lastCard = cardDeck[i] // define variables for shuffle
+    cardDeck[i] = cardDeck[randomLocation]
+    cardDeck[randomLocation] = lastCard
+    console.log(lastCard)
+  }
+  console.log('All cards shuffled')
+}
+
+const dealCard = () => {
+  const firstCard = cardDeck[0]
+  console.log(firstCard)
+  document.querySelector('.output').textContent =
+    firstCard.rank +
+    ' of ' +
+    firstCard.suit +
+    ' has a value of ' +
+    firstCard.value
+  console.log('does this do anything?' + { firstCard })
+}
+
 document.addEventListener('DOMContentLoaded', main)
 document.addEventListener('DOMContentLoaded', createDeck)
+document.addEventListener('DOMContentLoaded', shuffle)
+document.querySelector('.hit').addEventListener('click', dealCard)
